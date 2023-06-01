ecb4e92102b46926c7ee8916760341e57c99945a
GiproveR <127661085+GiproveR@users.noreply.github.com>
2023-06-01 16:56:04 +0800

    Homework1.java

--- Homework1/Homework1.java
+++ Homework1/Homework1.java
@@ -1,16 +1,16 @@
 public class Homework1 {
     public static void main (String[] args) {
-        for (int i=1; i<=100;i++) {
-            if (i%3==0&i%5==0) {
+        for (int i=1; i<=100;i++) { //цикл затрагивает диапазон [1,100]
+            if (i%3==0&i%5==0) { //выполняется, если число кратно 3 и 5
                 System.out.println("FizzBuzz");
             }
-            else if(i%3==0) {
+            else if(i%3==0) { //выполняется, если число кратно 3
                 System.out.println("Fizz");
             }
-            else if (i%5==0) {
+            else if (i%5==0) { //выполняется, если число кратно 5
                 System.out.println("Buzz");
             }
-            else {
+            else { //если условия выше не выполнились, выводится само число
                 System.out.println(i);
             }
         }
